3bc9f1d5d2dca71f6d69cf042f58dfe83ec108d3
romain groult <romain.groult@etu.u-paris.fr>
2026-01-04 13:29:21 +0100

    Main : implemented multiple commands

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,23 +2,40 @@ package org.example;
 
 import org.example.model.rdfs.NameExtractors;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Writer;
+import java.io.*;
 import java.lang.reflect.InvocationTargetException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import static org.example.service.ClassLoader.loadClass;
 import static org.example.service.CsvReader.readFile;
+import static org.example.service.TtlImporter.importFrom;
 import static org.example.service.TtlWriter.writeRdfsModel;
 
 public class Main {
 
     public static void main(String[] args) throws IOException, NoSuchFieldException, ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
-        String csvFilePath = args[0];
-        String classFilePath = args[1];
+        String command = args[0];
+        switch (command) {
+            case "csv" :
+                generateTurtleFromCsv(args[1], args[2], args[3]);
+                break;
+            case "request" :
+                generateTurtleFromRequest(args[1], args[2], args[3]);
+                break;
+        }
+    }
+
+    private static void generateTurtleFromRequest(String url, String requestPath, String target) throws IOException {
+        String request = new String(Files.readAllBytes(Paths.get(requestPath)));
+        Writer writer = new FileWriter(target, false);
+        importFrom(url, request, writer);
+    }
+
+
+    private static void generateTurtleFromCsv(String csvFilePath, String classFilePath, String target) throws IOException, ClassNotFoundException, NoSuchFieldException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         Class<?> clazz = loadClass(classFilePath);
-        File file = new File("out.ttl");
+        File file = new File(target);
         Writer writer = new FileWriter(file);
         writeRdfsModel(clazz, readFile(csvFilePath, clazz),"", NameExtractors.NAME_FIELD, writer);
         writer.close();
--- src/main/java/org/example/service/TtlImporter.java
+++ src/main/java/org/example/service/TtlImporter.java
@@ -3,36 +3,18 @@ package org.example.service;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.sparql.exec.http.QueryExecutionHTTP;
 
+import java.io.Writer;
+
 public class TtlImporter {
 
 
-    public static void main(String[] args) {
-        String serviceURL = "https://dbpedia.org/sparql";
-
-        String queryString = """
-                PREFIX dbo: <http://dbpedia.org/ontology/>
-                PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-                
-                CONSTRUCT {
-                  ?film a dbo:Film ;
-                        rdfs:label ?title ;
-                        dbo:director ?director .
-                }
-                WHERE {
-                  ?film a dbo:Film ;
-                        rdfs:label ?title ;
-                        dbo:director ?director .
-                  FILTER (lang(?title) = 'en')
-                }
-                LIMIT 1000
-                """;
-
+    public static void importFrom(String url, String query, Writer target){
         Model model = QueryExecutionHTTP.create()
-                .endpoint(serviceURL)
-                .query(queryString)
+                .endpoint(url)
+                .query(query)
                 .construct();
 
-        model.write(System.out, "TURTLE");
+        model.write(target, "TURTLE");
     }
 
 }
