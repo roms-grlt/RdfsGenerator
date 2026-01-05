fd2e5d0f94bb90561a3633c64d3f1a9c47e68c4c
Alban Talagrand <alban.talagrand@gmail.com>
2026-01-05 21:59:18 +0100

    request rework

---
+++ scripts/run.bat
@@ -0,0 +1,10 @@
+java -jar ../result/rdfs-generator.jar query ../queries/aggregation-request.sparql ../result/integrated.ttl
+java -jar ../result/rdfs-generator.jar query ../queries/complex-request.sparql ../result/integrated.ttl
+java -jar ../result/rdfs-generator.jar query ../queries/federated-request.sparql ../result/integrated.ttl
+java -jar ../result/rdfs-generator.jar query ../queries/minus-request.sparql ../result/integrated.ttl
+java -jar ../result/rdfs-generator.jar query ../queries/not-exists-request.sparql ../result/integrated.ttl
+java -jar ../result/rdfs-generator.jar query ../queries/optional-request.sparql ../result/integrated.ttl
+java -jar ../result/rdfs-generator.jar query ../queries/path-request.sparql ../result/integrated.ttl
+java -jar ../result/rdfs-generator.jar query ../queries/sources-request.sparql ../result/integrated.ttl
+java -jar ../result/rdfs-generator.jar query ../queries/sub-request-request.sparql ../result/integrated.ttl
+pause
--- src/main/java/org/example/service/QueryExecutor.java
+++ src/main/java/org/example/service/QueryExecutor.java
@@ -37,7 +37,18 @@ public class QueryExecutor {
                 System.out.print("|\t");
                 for (String param : queryParams) {
                     Object result = solution.get(param);
-                    System.out.print("\t" + param + " : " + (result == null ? "N/A" : result.toString()) + " \t|");
+                    String displayValue;
+
+                    if (result == null) {
+                        displayValue = "N/A";
+                    } else if (result instanceof org.apache.jena.rdf.model.Literal) {
+                        // Pour les littéraux, extraire uniquement la valeur sans le type
+                        displayValue = ((org.apache.jena.rdf.model.Literal) result).getLexicalForm();
+                    } else {
+                        displayValue = result.toString();
+                    }
+
+                    System.out.print("\t" + param + " : " + displayValue + " \t|");
                 }
                 System.out.print("\n");
             }
@@ -45,7 +56,7 @@ public class QueryExecutor {
     }
 
     public static void main(String[] args) throws IOException {
-        String request = new String(Files.readAllBytes(Paths.get("./result/requests/sub-request-request.txt")));
+        String request = new String(Files.readAllBytes(Paths.get("./queries/sub-request-request.sparql")));
         QueryExecutor.executeQuery(request, "./result/integrated.ttl");
 
     }
